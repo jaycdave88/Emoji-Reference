fe45863220b8e67af5d1630ce29dbf94ff7e8f91
Jay Dave <jaycdave@gmail.com>
2015-06-04 20:16:41 -0700

    first commit - pushing to github - added readme

---
+++ Emojis/EmojiListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiListViewController.swift
+//  Emojis
+//
+//  Created by DEV MODE on 6/4/15.
+//  Copyright (c) 2015 DEV MODE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EmojiViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = UIColor.blueColor()
+    }
+    
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+###Emoji Reference
+* A dictonary reference for diffrent emojis on diffrent devices 
+
+>Jay C. Davé
